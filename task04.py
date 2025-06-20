d74fd50177fd7894fbfeb3d9ba5016b33a830715
dvankevich <dvankevich@gmail.com>
2025-06-20 12:35:36 +0300

    task04

--- task04.py
+++ task04.py
@@ -1,9 +1,4 @@
-"""
-побудуйте функцію, що буде візуалізувати бінарну купу
-"""
-
 import uuid
-
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -19,9 +14,7 @@ class Node:
 
 def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
-        graph.add_node(
-            node.id, color=node.color, label=node.val
-        )  # Використання id та збереження значення вузла
+        graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2**layer
@@ -35,30 +28,33 @@ def add_edges(graph, node, pos, x=0, y=0, layer=1):
     return graph
 
 
-def draw_tree(tree_root):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    tree = add_edges(tree, tree_root, pos)
+def draw_heap(heap_root):
+    heap = nx.DiGraph()
+    pos = {heap_root.id: (0, 0)}
+    heap = add_edges(heap, heap_root, pos)
 
-    colors = [node[1]["color"] for node in tree.nodes(data=True)]
-    labels = {
-        node[0]: node[1]["label"] for node in tree.nodes(data=True)
-    }  # Використовуйте значення вузла для міток
+    colors = [node[1]["color"] for node in heap.nodes(data=True)]
+    labels = {node[0]: node[1]["label"] for node in heap.nodes(data=True)}
 
     plt.figure(figsize=(8, 5))
     nx.draw(
-        tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors
+        heap, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors
     )
+    plt.title("Binary Heap Visualization")
     plt.show()
 
 
-# Створення дерева
-root = Node(0)
-root.left = Node(4)
-root.left.left = Node(5)
-root.left.right = Node(10)
-root.right = Node(1)
-root.right.left = Node(3)
+# Створення бінарної купи
+def create_heap(arr):
+    nodes = [Node(key) for key in arr]
+    for i in range(len(nodes) // 2):
+        if 2 * i + 1 < len(nodes):
+            nodes[i].left = nodes[2 * i + 1]
+        if 2 * i + 2 < len(nodes):
+            nodes[i].right = nodes[2 * i + 2]
+    return nodes[0]  # Повертаємо корінь купи
 
-# Відображення дерева
-draw_tree(root)
+
+heap_array = [10, 9, 8, 6, 7, 5, 4]
+heap_root = create_heap(heap_array)
+draw_heap(heap_root)
